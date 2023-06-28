5d85b220fbf5ffb0dc11e3c027299e7294d10992
Ryel Banfield <ryelbanfield@gmail.com>
2023-06-28 19:21:00 -0400

    Create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,44 @@ const Home = async () => {
   const imageUrls = (await imageUrlsResponse.json()) as FileObject[];
 
   return (
-    <main className="min-h-screen px-3">
+    <div className="min-h-screen px-3">
       <header className="p-6">
         <h1 className="text-center text-2xl font-bold">SRM Gallery</h1>
       </header>
 
-      <Modal />
+      <main className="mb-6">
+        <Modal />
 
-      <Grid urlEndpoint="https://ik.imagekit.io/zuge4mgxf" images={imageUrls} />
-    </main>
+        <Grid
+          urlEndpoint="https://ik.imagekit.io/zuge4mgxf"
+          images={imageUrls}
+        />
+      </main>
+
+      <footer className="p-12">
+        <p className="text-center text-sm">
+          Built with 🔥 by{" "}
+          <a
+            href="https://ryelbanfield.me"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-bold underline"
+          >
+            Ryel
+          </a>{" "}
+          at{" "}
+          <a
+            href="https://rcbsoftware.co"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-bold underline"
+          >
+            RCB Software
+          </a>
+          .
+        </p>
+      </footer>
+    </div>
   );
 };
 
