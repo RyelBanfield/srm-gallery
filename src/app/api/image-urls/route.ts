4702d7fdf0c3163d179c45455a81355636a21985
Ryel Banfield <ryelbanfield@gmail.com>
2023-06-28 18:27:08 -0400

    Create api to fetch images

---
+++ src/app/api/image-urls/route.ts
@@ -0,0 +1,16 @@
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const urlEndpoint = "https://ik.imagekit.io/zuge4mgxf";
+
+  const imagekit = new ImageKit({
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY as string,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY as string,
+    urlEndpoint,
+  });
+
+  const imageUrls = await imagekit.listFiles({});
+
+  return NextResponse.json(imageUrls);
+}
