10bc21c5396080d45220c9622069951613104acb
Ryel Banfield <ryelbanfield@gmail.com>
2023-06-28 18:27:24 -0400

    Move fetching to api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,21 @@
-import ImageKit from "imagekit";
+import { type FileObject } from "imagekit/dist/libs/interfaces";
 
 import Grid from "@/components/Grid";
 import Modal from "@/components/Modal";
 
 const Home = async () => {
-  const urlEndpoint = "https://ik.imagekit.io/zuge4mgxf";
-
-  const imagekit = new ImageKit({
-    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY as string,
-    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY as string,
-    urlEndpoint,
-  });
-
-  const imageUrls = await imagekit.listFiles({});
+  const imageUrlsResponse = await fetch("http://localhost:3000/api/image-urls");
+  const imageUrls = (await imageUrlsResponse.json()) as FileObject[];
 
   return (
-    <main className="flex flex-col items-center justify-center px-3">
+    <main className="min-h-screen px-3">
       <header className="p-6">
         <h1 className="text-center text-2xl font-bold">SRM Gallery</h1>
       </header>
 
       <Modal />
 
-      <Grid urlEndpoint={urlEndpoint} images={imageUrls} />
+      <Grid urlEndpoint="https://ik.imagekit.io/zuge4mgxf" images={imageUrls} />
     </main>
   );
 };
